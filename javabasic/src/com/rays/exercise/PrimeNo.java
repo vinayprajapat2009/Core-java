f83e4ee137c190e0ba52b766e92c9f62c01b0dc2
vinayprajapat2009 <shubashprajapat68@gmail.com>
2026-06-11 18:53:54 +0530

    done

--- javabasic/src/com/rays/exercise/PrimeNo.java
+++ javabasic/src/com/rays/exercise/PrimeNo.java
@@ -2,21 +2,24 @@ package com.rays.exercise;
 
 public class PrimeNo {
 	public static void main(String[] args) {
-
-		int a = 13;
-		int count = 0;
-
-		for (int i = 2; i < a; i++) {
-			if (a % i == 0) {
-				count++;
-			}
-		}
-		System.out.println(count);
-		if(count > 0) {
-			System.out.println("not prime");
-		} else {
-			System.out.println("prime");
-		}
+		
+	  int a = 18;
+	  int count = 0;
+	  
+	  for(int i = 2; i < a; i++) {
+		  if(a % i == 0) {
+			  count ++;
+			  
+		  }
+	  }
+			  System.out.println(count);
+			  if(count > 0) {
+				  System.out.println("not prime");
+			  }else {
+				  System.out.println("prime");
+			  }
+		  
+	  
 
 	}
 }
---
+++ javabasic/src/com/rays/string/TestString.java
@@ -0,0 +1,21 @@
+package com.rays.string;
+
+public class TestString {
+	
+	public static void main(String[] args) {
+		
+		String name = "vinay prajapat";
+		
+		System.out.println(name);
+		System.out.println(name.length());
+		System.out.println(name.charAt(6));
+		System.out.println(name.indexOf('a'));
+		System.out.println(name.lastIndexOf('a'));
+		System.out.println(name.toUpperCase());
+		System.out.println(name.toLowerCase()); 
+		System.out.println(name.startsWith("vin"));
+		System.out.println(name.endsWith("Pat"));
+		System.out.println(name.substring(1, 7));
+	}
+
+}
